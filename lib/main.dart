afd3aa1185a3ac41c924aa7592f6701a8d6d267b
bruno pf silva <bruno.pfsilva@hotmail.com>
2019-11-15 15:03:13 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:app_chat/pages/home/HomeWidget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  MaterialApp(
+    home: homeWidget(),
+  );
+}
+
+
+
---
+++ lib/pages/home/HomeWidget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class homeWidget extends StatefulWidget {
+  @override
+  _homeWidgetState createState() => _homeWidgetState();
+}
+
+class _homeWidgetState extends State<homeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
